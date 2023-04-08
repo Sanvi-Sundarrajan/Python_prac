efe845800134e2c5c445f36109aa507190ba0a84
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2023-04-08 09:41:55 +0000

    coded

--- main.py
+++
@@ -1,7 +0,0 @@
-sample={"hello":"world","hi":"earth","chemistry":"hate"}
-#for i in sample:
-#		print(i,sample[i])
-sample.get()
-	#print(sample[i]) will give all the values
-	#print(i) will give key
-#print(sample) without the loop will print the dictionery
