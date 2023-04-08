dc66a7d144bad50255083d3cfc6e07ac0293bc7f
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2023-04-08 09:41:18 +0000

    coded

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n=int(input("enter the range "))
-ls=[]
-for i in range (n) :
-	ls.append(i)
-print(ls)	
+sample={"hello":"world","hi":"earth","chemistry":"hate"}
+#for i in sample:
+#		print(i,sample[i])
+sample.get()
+	#print(sample[i]) will give all the values
+	#print(i) will give key
+#print(sample) without the loop will print the dictionery
